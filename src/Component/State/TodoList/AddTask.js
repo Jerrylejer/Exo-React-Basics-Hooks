475e5d96d557d94f2592138f7704e96cd9078013
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-04-06 18:26:10 +0200

    finalisation commentaires

--- src/Component/State/TodoList/AddTask.js
+++ src/Component/State/TodoList/AddTask.js
@@ -3,12 +3,15 @@ import React, { useState } from 'react';
 const AddTask = ({ addNewTask }) => {
 
     const [newTask, setNewTask] = useState('');
+
 //todo (4) Je crée la fonction qui ajoute la nouvelle tâche
     const submitTask = (e) => {
         e.preventDefault();
         // console.log('ok');
         //todo (5) Je récupère "addNewTask" et lui passe le paramètre "newTask"
         addNewTask(newTask);
+        // Je vide l'input
+        setNewTask('')
     };
 
     const addOneTask = (e) => {
@@ -18,7 +21,7 @@ const AddTask = ({ addNewTask }) => {
         setNewTask(task);
         //todo (1) => Je récupère la valeur => Il faut la passer à TodoList maintenant
     };
-    
+
     //todo (6) => "submitTask" se lance au submit du formulaire
     return (
         <>
--- src/Component/State/TodoList/TodoList.js
+++ src/Component/State/TodoList/TodoList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import AddTask from "./AddTask";
+import AddTask from './AddTask';
 
 const TodoList = () => {
     const [task, setTask] = useState([
@@ -11,27 +11,56 @@ const TodoList = () => {
         { id: 5, task: 'Réviser le useMemo' },
     ]);
 
-//todo (2) je charge "setTask" avec la liste existante (spread) + le nouvel objet (en paramètre la nouvelle tâche)
-const addNewTask = (newTask) => {
-    setTask([
-        ...task, {
-            id: uuidv4(),
-            task: newTask
-        }
-    ])
+    // State qui gère l'input vide à la soumission
+    const [empty, setEmpty] = useState(false);
 
-}
-//todo (3) je passe la fonction "addNewTask" en props à <AddTask /> 
+    //todo (2) je charge "setTask" avec la liste existante (spread) + le nouvel objet (en paramètre la nouvelle tâche)
+    const addNewTask = (newTask) => {
+
+        //* 1-condition qui déclenche ou non la fonction de nouvelle tâche lors du submit
+        if (newTask !== '') {
+            //* 2-Chaine de caractère dans l'input => Fonction ok et state "empty" s'initialise à false
+            setEmpty(false);
+
+            setTask([
+                ...task,
+                {
+                    id: uuidv4(),
+                    task: newTask,
+                },
+            ]);
+        } else {
+            //* 3-input vide => Fonction bloquée, pas de nouvelle tache, state "empty" s'initialise à true
+            setEmpty(true);
+        }
+    };
+
+    //* 4-initialisation d'une variable "emptyWarning" qui instancie une alerte dans le DOM
+    const emptyWarning = empty && (
+        <div class='alert alert-danger' role='alert'>
+            Il faut indiquer une nouvelle tâche !
+        </div>
+    );
+
+    //todo (3) je passe la fonction "addNewTask" en props à <AddTask />
     return (
         <div>
-            <p><strong>TodoList:</strong> Nous avons {task.length} tâches à effectuer</p>
-            <ul className="list-group">
+            <p>
+                <strong>TodoList:</strong> Nous avons {task.length} tâches à
+                effectuer
+            </p>
+            {emptyWarning}
+            <ul className='list-group'>
                 {task.map((item) => {
-                    return <li key={item.id} className="list-group-item">{item.task}</li>;
+                    return (
+                        <li key={item.id} className='list-group-item'>
+                            {item.task}
+                        </li>
+                    );
                 })}
             </ul>
             <br />
-            <AddTask addNewTask={addNewTask}/>
+            <AddTask addNewTask={addNewTask} />
         </div>
     );
 };
